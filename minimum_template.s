af2c706493ab3aac05113f44033f3d30abc1c141
Vaishnavinara7 <82509266+Vaishnavinara7@users.noreply.github.com>
2022-09-12 19:18:38 -0700

    Update minimum_template.s

--- minimum_template.s
+++ minimum_template.s
@@ -170,7 +170,7 @@ sloop:
 		add $t0, $t0, $a0
 
             # [$t2=V[$t0]] Load the value of memory address $t0 to $t2, $t2 = V[MaxIndex]
-		lw $t2, $t0
+		lw $t2, A($t0)
 
             # [$t1=$s0-1] The index (n - 1) that will be swapped with MaxIndex
 		add $t1, $s0, $0
@@ -183,13 +183,13 @@ sloop:
 		add $t1, $t1, $a0
 
             # [$t3=V[$t1]] Load the value of memory address $t1 to $t3, $t3 = V[n - 1]
-		lw $t3, $t1
+		lw $t3, A($t1)
 
             # [V[$t1]=$t2] Store V[n-1] to be V[MaxIndex]
-		sw $t2, $t1
+		sw $t2, A($t1)
 
             # [V[$t0]=$t3] Store V[MaxIndex] to be the original V[n - 1]
-		sw $t3, $t0
+		sw $t3, A($t0)
 
             # [$s0=$s0-1] Len = Len - 1 
 		addi $s0, $s0, -1
